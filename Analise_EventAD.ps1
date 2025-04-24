5076dda3867cfe755875d068f9bcb10d00878e7f
Thiago Passamani <thiagopassamani@gmail.com>
2025-04-24 13:06:20 -0300

    Update Analise_EventAD.ps1

--- Analise_EventAD.ps1
+++ Analise_EventAD.ps1
@@ -2,13 +2,13 @@
 # Em deseolvimento.
 ###############################################################################################################################
 # Quantidade de eventos
-$MaxEventsN = 1000
+$MaxEventsN = 100
 
 # Definir caminho do arquivo HTML
-$HtmlFile = "C:\Tools\Analise_EventAD.html"
+$HtmlFile = "C:\Tools\Falhas_Login_AD.html"
 
 # Lista de servidores para consultar eventos
-$Servers = @("SERVER1.LOCAL", "SERVER2.LOCAL")
+$Servers = @("DC18.NOVAFORMA.LOCAL", "NF-AZ-AD-01.NOVAFORMA.LOCAL")
 
 # Definir intervalo de tempo para a data atual
 $TodayStart = Get-Date -Hour 00 -Minute 00 -Second 00
@@ -18,14 +18,19 @@ $TodayEnd = Get-Date -Hour 23 -Minute 59 -Second 59
 $AllEvents = @()
 
 foreach ($Server in $Servers) {
-
+    
+    Write-Host "[+] Consultando eventos no servidor: " -ForegroundColor Yellow
+    
     try {
-        Write-Host "[+] Consultando eventos no servidor: $Server" -ForegroundColor Yellow
+               
+        # Buscar eventos de falha de login
+        $Events = Get-WinEvent -LogName Security -ComputerName $Server -MaxEvents $MaxEventsN | Where-Object { $_.TimeCreated -ge $TodayStart -and $_.TimeCreated -le $TodayEnd -and ($_.Id -eq 4624 -or $_.Id -eq 4625 -or $_.Id -eq 4740) } 
         
-        # Buscar eventos de falha de login (ID 4625)
-        $Events = Get-WinEvent -LogName Security -ComputerName $Server -MaxEvents $MaxEventsN | Where-Object { $_.TimeCreated -ge $TodayStart -and $_.TimeCreated -le $TodayEnd -and $_.Id -eq 4624 -or $_.Id -eq 4625 -or $_.Id -eq 4740 } 
+        Write-Host "[*] $Server" -ForegroundColor Green
         
         if ($Events.Count -gt 0) {
+            Write-Host "[*] Analisando eventos em $Server." -ForegroundColor Green
+
             foreach ($Event in $Events) {
                 $Message = $Event.Message
                 $UserName = "Desconhecido"
@@ -35,18 +40,19 @@ foreach ($Server in $Servers) {
                 }
 
                 # Adiciona evento ao array
-                $AllEvents += [PSCustomObject]@{
+                $AllEvents += [PSCustomObject]@{                    
                     Servidor = $Server
-                    Usuario = $UserName
+                    IdEventN = $Event.Id
                     DataHora = $Event.TimeCreated
                     Detalhes = $Message
                 }
             }
         } else {
-            Write-Host "[-] Nenhum evento encontrado hoje no servidor: $Server" -ForegroundColor Red
+            Write-Host "[*] Nenhum evento encontrado no $Server" -ForegroundColor Yellow
         }
+        Write-Host "[*] Finalizando..." -ForegroundColor Green
     } catch {
-        Write-Host "[-] Erro ao conectar-se ao servidor: $Server" -ForegroundColor Red
+        Write-Host "[-] Erro ao conectar-se ao $Server" -ForegroundColor Red
     }
 }
 
@@ -60,7 +66,6 @@ $HtmlContent = @"
     <title>Relatório de Bloqueios de Conta</title>
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css' rel='stylesheet'>
     <script src='https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js'></script>
-    <!-- <script src='https://cdn.jsdelivr.net/npm/jsPDF@2.5.1/dist/jspdf.umd.min.js'></script>-->
 </head>
 <body class='container mt-4'>
     <h2 class='text-center'>Relatório de Bloqueios de Conta no Active Directory</h2>
@@ -68,15 +73,12 @@ $HtmlContent = @"
     <div class='mb-3'>
         <input type='text' id='filterInput' class='form-control' placeholder='Digite...' onkeyup='filterTable()'>
     </div>
-    <!--
-    <button class='btn btn-success' onclick='exportToCSV()'>Exportar CSV</button>
-    <button class='btn btn-danger' onclick='exportToPDF()'>Exportar PDF</button>
-    -->
+
     <table class='table table-striped table-bordered mt-3' id='dataTable' data-url="json/data1.json" data-filter-control="true" data-show-search-clear-button="true">
         <thead class='table-dark'>
-            <tr>
+            <tr>                
                 <th>Servidor</th>
-                <!--<th>Falha</th>-->
+                <th>ID</th>
                 <th>Data e Hora</th>
                 <th>Detalhes</th>
             </tr>
@@ -90,7 +92,7 @@ foreach ($Event in $AllEvents) {
     $HtmlContent += @"
         <tr>
             <td>$($Event.Servidor)</td>
-            <!--<td>$($Event.Usuario)</td>-->
+            <td>$($Event.IdEventN)</td>
             <td>$($Event.DataHora)</td>
             <td class='filter'>$($Event.Detalhes)</td>
         </tr>
@@ -112,42 +114,6 @@ $HtmlContent += @"
                 row.style.display = rowText.includes(input) ? "" : "none";
             });
         }
-
-        /**
-        function exportToCSV() {
-            let csv = 'Usuário,Data e Hora,Detalhes\\n';
-            let rows = document.querySelectorAll('#dataTable tbody tr');
-            
-            rows.forEach(row => {
-                let data = [];
-                row.querySelectorAll('td').forEach(cell => data.push(cell.innerText));
-                csv += data.join(',') + '\\n';
-            });
-            
-            let blob = new Blob([csv], { type: 'text/csv' });
-            let a = document.createElement('a');
-            a.href = URL.createObjectURL(blob);
-            a.download = 'Bloqueios_AD.csv';
-            a.click();
-        }
-
-        function exportToPDF() {
-            let { jsPDF } = window.jspdf;
-            let doc = new jsPDF();
-            doc.text('Relatório de Bloqueios de Conta', 10, 10);
-            
-            let rows = document.querySelectorAll('#dataTable tbody tr');
-            let y = 20;
-            rows.forEach(row => {
-                let data = [];
-                row.querySelectorAll('td').forEach(cell => data.push(cell.innerText));
-                doc.text(data.join(' | '), 10, y);
-                y += 10;
-            });
-
-            doc.save('Bloqueios_AD.pdf');
-        }
-        */
     </script>
 
     <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js'></script>
@@ -159,4 +125,5 @@ $HtmlContent += @"
 # Salvar arquivo HTML
 $HtmlContent | Out-File -Encoding utf8 $HtmlFile
 
-Write-Host "Relatório gerado com eventos de falha de login (ID 4625) em múltiplos servidores!" -ForegroundColor Green
+Write-Host "[+] Relatório gerado com eventos de falha de login em múltiplos servidores!" -ForegroundColor Green
+
